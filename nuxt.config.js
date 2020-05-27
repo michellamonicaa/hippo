0d96950b23b06d2c6ba790858bbee78995fecaaa
Michella Monica <michellamonica@192.168.8.11>
2020-05-28 01:47:43 +0700

    ler1

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'hi',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -14,7 +14,80 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'sytlesheet',
+        crossorigin: 'anonymous',
+        href:
+          'https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/fonts/icomoon/style.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/bootstrap.min.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/magnific-popup.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/jquery-ui.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/owl.carousel.min.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/owl.theme.default.min.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/bootstrap-datepicker.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/fonts/flaticon/font/flaticon.css',
+        type: 'text/css'
+      },
+      {
+        rel: 'sytlesheet',
+        crossorigin: 'anonymous',
+        href:
+          'https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css'
+      },
+      {
+        rel: 'sytlesheet',
+        href: '/css/style.css',
+        type: 'text/css'
+      }
+    ],
+    script: [
+      { src: '/js/jquery-3.3.1.min.js', body: true },
+      { src: '/js/jquery-migrate-3.0.1.min.js', body: true },
+      { src: '/js/jquery-ui.js', body: true },
+      { src: '/js/popper.min.js', body: true },
+      { src: '/js/bootstrap.min.js', body: true },
+      { src: '/js/owl.carousel.min.js', body: true },
+      { src: '/js/jquery.stellar.min.js', body: true },
+      { src: '/js/jquery.countdown.min.js', body: true },
+      { src: '/js/jquery.magnific-popup.min.js', body: true },
+      { src: '/js/bootstrap-datepicker.min.js', body: true }
+      // { src: '/js/aos.js', body: true }
+      // { src: '/js/main.js', body: true }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -56,6 +129,10 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+    }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,165 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        travel
-      </h1>
-      <h2 class="subtitle">
-        website travel
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="site-wrap">
+      <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
       </div>
+
+      <header class="site-navbar py-1" role="banner">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-6 col-xl-2">
+              <h1 class="mb-0">
+                <a href="index.html" class="text-black h2 mb-0">Travelers</a>
+              </h1>
+            </div>
+            <div class="col-10 col-md-8 d-none d-xl-block">
+              <nav
+                class="site-navigation position-relative text-right text-lg-center"
+                role="navigation"
+              >
+                <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                  <li class="active">
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li class="has-children">
+                    <a href="destination.html">Destinations</a>
+                    <ul class="dropdown">
+                      <li>
+                        <a href="#">Japan</a>
+                      </li>
+                      <li>
+                        <a href="#">Europe</a>
+                      </li>
+                      <li>
+                        <a href="#">China</a>
+                      </li>
+                      <li>
+                        <a href="#">France</a>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li>
+                    <a href="discount.html">Discount</a>
+                  </li>
+                  <li>
+                    <a href="about.html">About</a>
+                  </li>
+                  <li>
+                    <a href="blog.html">Blog</a>
+                  </li>
+
+                  <li>
+                    <a href="contact.html">Contact</a>
+                  </li>
+                  <!-- <li><a href="booking.html">Book Online</a></li> -->
+                </ul>
+              </nav>
+            </div>
+
+            <div class="col-6 col-xl-2 text-right">
+              <div class="d-none d-xl-inline-block">
+                <ul
+                  class="site-menu js-clone-nav ml-auto list-unstyled d-flex text-right mb-0"
+                  data-class="social"
+                >
+                  <li>
+                    <a href="#" class="pl-0 pr-3 text-black">
+                      <span class="icon-tripadvisor"></span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="pl-3 pr-3 text-black">
+                      <span class="icon-twitter"></span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="pl-3 pr-3 text-black">
+                      <span class="icon-facebook"></span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="pl-3 pr-3 text-black">
+                      <span class="icon-instagram"></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div
+                class="d-inline-block d-xl-none ml-md-0 mr-auto py-3"
+                style="position: relative; top: 3px;"
+              >
+                <a href="#" class="site-menu-toggle js-menu-toggle text-black">
+                  <span class="icon-menu h3"></span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <client-only>
+        <div class="slide-one-item home-slider owl-carousel">
+          <div
+            class="site-blocks-cover overlay"
+            style="background-image: url(images/hero_bg_1.jpg);"
+          >
+            <div class="container">
+              <div
+                class="row align-items-center justify-content-center text-center"
+              >
+                <div class="col-md-8">
+                  <h1 class="text-white font-weight-light">
+                    Never Stop Exploring
+                  </h1>
+                  <p class="mb-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fuga est inventore ducimus repudiandae.
+                  </p>
+                  <p>
+                    <a href="#" class="btn btn-primary py-3 px-5 text-white"
+                      >Book Now!</a
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="site-blocks-cover overlay"
+            style="background-image: url(images/hero_bg_2.jpg);"
+          >
+            <div class="container">
+              <div
+                class="row align-items-center justify-content-center text-center"
+              >
+                <div class="col-md-8">
+                  <h1 class="text-white font-weight-light">Love The Places</h1>
+                  <p class="mb-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fuga est inventore ducimus repudiandae.
+                  </p>
+                  <p>
+                    <a href="#" class="btn btn-primary py-3 px-5 text-white"
+                      >Book Now!</a
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </client-only>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
